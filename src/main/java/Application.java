45dfb2be13b427a20e9790bb2bd6915cf6a07e43
LakoR15 <alexvolkov1994@gmail.com>
2015-08-25 21:10:10 +0300

    Добавлен логин пользователя

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,12 +8,8 @@ public class Application {
     public static void main(String[] args){
 
         int port;
-
-        if (args.length > 0){
-            port = Integer.parseInt(args[0]);
-        }else {
-            port = 5555;
-        }
+//        port = Integer.parseInt(args[0]);
+        port = 5555;
 
         port(port);
         new UserRoutes();
