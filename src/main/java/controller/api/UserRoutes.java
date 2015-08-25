4cd0d6073765b22e93f27a87957b57298c017e90
LakoR15 <alexvolkov1994@gmail.com>
2015-08-25 21:58:03 +0300

    Обновлена функция регистрации

--- src/main/java/controller/api/UserRoutes.java
+++ src/main/java/controller/api/UserRoutes.java
@@ -2,7 +2,6 @@ package controller.api;
 
 import com.google.gson.Gson;
 import controller.BaseController;
-import model.List;
 import model.User;
 
 
@@ -17,23 +16,39 @@ public class UserRoutes extends BaseController {
         post("/user/registration", (request, response) -> {
 
             User user = new Gson().fromJson(request.body(), User.class);
-
+            User user1 = null;
             //Тут должно быть хеширование пароля
 
             em = getEm();
             try {
                 em.getTransaction().begin();
-                em.persist(user);
+                user1 = em.createQuery("SELECT user FROM model.User user WHERE user.name=:name", User.class)
+                        .setParameter("name", user.getName())
+                        .getSingleResult();
                 em.getTransaction().commit();
             } catch (Exception e) {
                 em.getTransaction().rollback();
-                throw new RuntimeException("Error " + e.getMessage());
             } finally {
                 em.close();
             }
 
-            return user.getId().toString() + " " + user.getSecretKey();
+            if(user1 == null){
+                em = getEm();
+                try {
+                    em.getTransaction().begin();
+                    em.persist(user);
+                    em.getTransaction().commit();
+                } catch (Exception e) {
+                    em.getTransaction().rollback();
+                    throw new RuntimeException("Error " + e.getMessage());
+                } finally {
+                    em.close();
+                }
 
+                return user.getId().toString() + " " + user.getSecretKey();
+            }else {
+                return "Error 1";
+            }
         });
 
         post("/user/login", (request, response) -> {
